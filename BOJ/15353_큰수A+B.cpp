6f0cdff9403e029a3e4ae524e78821ae7a84c459
sunny <ishong@inu.ac.kr>
2020-11-06 23:57:14 +0900

    201106 up

---
+++ BOJ/15353_큰수A+B.cpp
@@ -0,0 +1,43 @@
+/**
+ * 20-11-06
+ * BOJ 15353 큰 수 A+B
+ * string + 수학
+ * */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string A, B;
+
+string solveSum(string num1, string num2){
+    long long sum=0;
+    string res="";
+
+    while(!num1.empty() || !num2.empty() || sum){
+        if(!num1.empty()){
+            sum += num1.back() - '0';
+            num1.pop_back();
+        }
+
+        if(!num2.empty()){
+            sum += num2.back() - '0';
+            num2.pop_back();
+        }
+
+        res.push_back(sum%10+'0');
+        sum/=10;
+    }
+    
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> A >> B;
+    cout << solveSum(A, B);
+    return 0;
+}
