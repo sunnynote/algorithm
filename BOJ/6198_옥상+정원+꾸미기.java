90869f4e5747304ac40e40fdf64e4063bd6a4593
Sunny <ishong@inu.ac.kr>
2021-12-02 17:57:43 +0900

    21-12-02

---
+++ BOJ/6198_옥상+정원+꾸미기.java
@@ -0,0 +1,33 @@
+/**
+ * 21-12-02
+ * BOJ 6198 옥상 정원 꾸미기
+ * STACK
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class p6198 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long ans = 0;
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<N; i++){
+            int cur = Integer.parseInt(br.readLine());
+
+            while(!st.isEmpty() && st.peek() <= cur){
+                st.pop();
+            }
+
+            st.push(cur);
+            ans += (st.size()-1);
+        }
+
+        System.out.println(ans);
+    }
+}
