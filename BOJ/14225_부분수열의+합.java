929f1e2037ed5993289afc32938003938f0c37ff
Sunny <ishong@inu.ac.kr>
2022-02-14 00:11:10 +0900

    22-02-13

--- BOJ/14225_부분수열의+합.java
+++ BOJ/14225_부분수열의+합.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class test{
 
@@ -10,23 +11,24 @@ public class test{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
         chk = new boolean[20 * 100000 + 1];
 
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
         solve(0, 0);
 
-        boolean f = false;
-        int idx = 0;
-        while(true){
-            if(f) break;
-
-            if(chk[idx++]){
-                System.out.println(idx);
-                f = true;
-            }
+        int idx = 1;
+        while(chk[idx]){
+            idx++;
         }
+
+        System.out.println(idx);
     }
 
     static void solve(int cnt, int sum) {
