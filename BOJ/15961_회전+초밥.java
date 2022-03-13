4fba99ef5ae2a2abbcd38386f55124bbce6449c9
sunny <inseon0731@naver.com>
2022-03-14 00:35:05 +0900

    22-03-14

--- BOJ/15961_회전+초밥.java
+++ BOJ/15961_회전+초밥.java
@@ -1,14 +1,19 @@
+/**
+ * 22-03-13
+ * BOJ 15961 회전 초밥
+ * 슬라이딩 윈도우
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class test2 {
-  static int N;
-  static int d, k, c;
+  static int N, d, k, c;
   static int[] arr;
   static int[] chk;
-  static int ans, mx;
+  static int ans = 0, eat = 0;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,10 +23,9 @@ public class test2 {
     d = Integer.parseInt(st.nextToken());
     k = Integer.parseInt(st.nextToken());
     c = Integer.parseInt(st.nextToken());
+
     arr = new int[N+k-1];
-    chk = new int[N+k-1];
-    ans = 0;
-    mx = 0;
+    chk = new int[d+1];
 
     for(int i=0; i<N; i++){
       arr[i] = Integer.parseInt(br.readLine());
@@ -38,17 +42,35 @@ public class test2 {
   }
 
   static void solve(){
+
+    // 처음 k개 먹어본다.
     for(int i=0; i<k; i++){
-      if(chk[arr[i]]==0) mx++;
+      if(chk[arr[i]]==0) eat++;
       chk[arr[i]]++;
     }
 
-    for(int i=1; i<N+k-1; i++){
+    ans = Math.max(ans, eat);
+    if(chk[c]==0)
+      ans++;
 
+    int idx = 0;
+    for(int i=k; i<N+k-1; i++){
+      // 앞 초밥 뱉기
+      if(chk[arr[idx]]==1){
+        eat--;
+      }
+      chk[arr[idx++]]--;
 
-      chk[arr[i-1]]--;
-
+      // 현재 초밥 먹기
+      if(chk[arr[i]]==0){
+        eat++;
+      }
       chk[arr[i]]++;
+
+      if(chk[c]==0)
+        ans = Math.max(ans, eat+1);
+      else
+        ans = Math.max(ans, eat);
     }
   }
 }
