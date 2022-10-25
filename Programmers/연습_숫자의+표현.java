805197002453247a0f121dc89932030c843cc245
sunny <inseon0731@naver.com>
2022-10-25 19:29:47 +0900

    22-10-25

---
+++ Programmers/연습_숫자의+표현.java
@@ -0,0 +1,27 @@
+/**
+ * 22-10-25
+ * Programmers 연습 - 숫자의 표현
+ */
+class Solution {
+  public int solution(int n) {
+    int answer = 0;
+
+    for(int i=1; i<=n; i++){
+
+      int sum = 0;
+
+      for(int j=i; j<=n; j++){
+        sum += j;
+
+        if(sum == n){
+          answer++;
+          break;
+        }
+        else if(sum > n){
+          break;
+        }
+      }
+    }
+    return answer;
+  }
+}
