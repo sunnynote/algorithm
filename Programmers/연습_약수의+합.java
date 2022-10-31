38e768fbd82eaba8f8fc4ec3229ca53a49bc169c
sunny <inseon0731@naver.com>
2022-10-31 22:07:40 +0900

    22-10-31

---
+++ Programmers/연습_약수의+합.java
@@ -0,0 +1,14 @@
+/**
+ * 22-10-31
+ * Programmers 연습 - 약수의 합
+ */
+
+class Solution {
+  public int solution(int n) {
+    int answer = 0;
+    for(int i=1; i<=n; i++){
+      if(n % i == 0) answer += i;
+    }
+    return answer;
+  }
+}
