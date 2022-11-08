fe899f73069b1634b95e54d46107df86dcf1c67c
sunny <inseon0731@naver.com>
2022-11-08 22:25:48 +0900

    22-11-08

---
+++ Programmers/연습_올바른+괄호 복사본.java
@@ -0,0 +1,17 @@
+/**
+* 22-11-08
+* Programmers 연습 - 자릿수 더하기
+*/
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
