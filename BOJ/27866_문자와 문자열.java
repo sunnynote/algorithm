fb5d801b088d4c1f0a11119659a1a495634c8f3b
sunny <inseon0731@naver.com>
2023-03-24 17:58:15 +0900

    23-03-24

---
+++ BOJ/27866_문자와 문자열.java
@@ -0,0 +1,16 @@
+/**
+ * 23-03-24
+ * BOJ 27866 문자와 문자열
+ */
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+열
+    String s = br.readLine();
+    int cnt = Integer.parseInt(br.readLine());
+
+    System.out.println(s.charAt(cnt-1));
+  }
+}
