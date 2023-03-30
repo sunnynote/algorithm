ac6d68eddb91c294e4fa8c94a579c494db646ab9
sunny <inseon0731@naver.com>
2023-03-30 22:26:53 +0900

    23-03-30

---
+++ BOJ/27323_직사각형.java
@@ -0,0 +1,15 @@
+/**
+ * 23-03-30
+ * BOJ 27323 직사각형
+ */
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
+    System.out.println(N * M);
+  }
+}
