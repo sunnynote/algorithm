27beeb0d41eee99fb01b9e52cdbc2ba7422f84e6
sunny <inseon0731@naver.com>
2023-06-13 21:35:40 +0900

    23-06-13

---
+++ BOJ/15000_CAPS.java
@@ -0,0 +1,15 @@
+/**
+ * 23-06-13
+ * BOJ 15000 CAPS
+ */
+
+import java.io.*;
+
+public class ㄴ {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s = br.readLine();
+
+    System.out.print(s.toUpperCase());
+  }
+}
