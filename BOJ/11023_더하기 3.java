5e24bcb17ff321014a3703ba972df81af449ea63
sunny <inseon0731@naver.com>
2023-07-18 20:34:16 +0900

    23-07-18

---
+++ BOJ/11023_더하기 3.java
@@ -0,0 +1,25 @@
+/**
+ * 23-07-18
+ * BOJ 11023 더하기 3
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = st.countTokens();
+    int ans = 0;
+
+    for(int i=0; i<N; i++){
+      ans += Integer.parseInt(st.nextToken());
+    }
+
+    System.out.println(ans);
+  }
+}
